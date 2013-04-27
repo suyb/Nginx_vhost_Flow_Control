a19a569b6cda2ab0e11d8500fad60a2ad43e1677
suyb <suyb381@gmail.com>
2013-04-27 12:12:13 +0800

    Create calc_Nginx_log.sh

---
+++ calc_Nginx_log.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#You must install webalizer.
+
+to_path='/var/log/admin_nginx/logs'
+bin_path='/usr/local/webserver/webalizer/bin'
+record_path='/var/log/admin_nginx'
+
+$bin_path/webalizer $to_path/access_hostname1.log -o $record_path/hostname1/ > /dev/null
+$bin_path/webalizer $to_path/access_hostname2.log -o $record_path/hostname2/ > /dev/null
+echo "------calc_Nginx_log.sh is ok.  $(date +%F) $(date +%X------)" >> /var/log/admin_nginx/report.log
+echo "  " >> /var/log/admin_nginx/report.log 
